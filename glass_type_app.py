5b6e49983327930f51c60936a071e3cbbbb48c28
AnDrewMountain <108286028+AnDrewMountain@users.noreply.github.com>
2022-07-07 10:04:14 -0500

    Add files via upload

---
+++ glass_type_app.py
@@ -0,0 +1,153 @@
+# Importing the necessary Python modules.
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score 
+
+# ML classifier Python modules
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+# Loading the dataset.
+@st.cache()
+def load_data():
+    file_path = "glass-types.csv"
+    df = pd.read_csv(file_path, header = None)
+    # Droping the 0th column as it contains only the serial numbers.
+    df.drop(columns = 0, inplace = True)
+    column_headers = ['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'GlassType']
+    columns_dict = {}
+    # Renaming columns with suitable column headers.
+    for i in df.columns:
+        columns_dict[i] = column_headers[i - 1]
+        # Rename the columns.
+        df.rename(columns_dict, axis = 1, inplace = True)
+
+    return df
+
+glass_df = load_data() 
+
+# Creating the features data-frame holding all the columns except the last column.
+X = glass_df.iloc[:, :-1]
+
+# Creating the target series that holds last column.
+y = glass_df['GlassType']
+
+# Spliting the data into training and testing sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+@st.cache()
+def prediction(model, ri, na, mg, al, si, k, ca, ba, fe):
+    glass_type = model.predict([[ri, na, mg, al, si, k, ca, ba, fe]])
+    glass_type = glass_type[0]
+    if glass_type == 1:
+        return "building windows float processed".upper()
+    elif glass_type == 2:
+        return "building windows non float processed".upper()
+    elif glass_type == 3:
+        return "vehicle windows float processed".upper()
+    elif glass_type == 4:
+        return "vehicle windows non float processed".upper()
+    elif glass_type == 5:
+        return "containers".upper()
+    elif glass_type == 6:
+        return "tableware".upper()
+    else:
+        return "headlamps".upper()
+
+# Add title on the main page and in the sidebar.
+st.title("Glass Type Predictor")
+st.sidebar.title("Exploratory Data Analysis")
+
+# Using if statement, display raw data on the click of the checkbox.
+if st.sidebar.checkbox("Show raw data"):
+    st.subheader("Full Dataset")
+    st.dataframe(glass_df)
+
+# Sidebar for scatter plot.
+st.sidebar.subheader("Scatter Plot")
+
+# Choosing x-axis values for scatter plots.
+features_list = st.sidebar.multiselect("Select the x-axis values:", 
+                                            ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'))
+
+# Remove deprecation warning.
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Create scatter plots.
+for feature in features_list:
+    st.subheader(f"Scatter plot between {feature} and GlassType")
+    plt.figure(figsize = (12, 6))
+    sns.scatterplot(x = feature, y = 'GlassType', data = glass_df)
+    st.pyplot()
+
+# Sidebar for histograms.
+st.sidebar.subheader("Histogram")
+
+# Choosing features for histograms.
+hist_features = st.sidebar.multiselect("Select features to create histograms:", 
+                                            ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'))
+# Create histograms.
+for feature in hist_features:
+    st.subheader(f"Histogram for {feature}")
+    plt.figure(figsize = (12, 6))
+    plt.hist(glass_df[feature], bins = 'sturges', edgecolor = 'black')
+    st.pyplot() 
+
+# Sidebar for box plots.
+st.sidebar.subheader("Box Plot")
+
+# Choosing columns for box plots.
+box_plot_cols = st.sidebar.multiselect("Select the columns to create box plots:",
+                                            ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'GlassType'))
+
+# Create box plots.
+for col in box_plot_cols:
+    st.subheader(f"Box plot for {col}")
+    plt.figure(figsize = (12, 2))
+    sns.boxplot(glass_df[col])
+    st.pyplot()
+
+st.sidebar.subheader("Choose Classifier: ")
+classifier = st.sidebar.selectbox("Classifier ", ("Support Vector Mechine", "Random Forest Classifier", "Logistic Regression"))
+if classifier == 'Support Vector Machine':
+    st.sidebar.subheader("Model Hyperparameters")
+    c_value = st.sidebar.number_input("C (Error Rate)", 1, 100, step = 1)
+    kernel_input = st.sidebar.radio("Kernel", ("linear", "rbf", "poly"))
+    gamma_input = st. sidebar.number_input("Gamma", 1, 100, step = 1)
+
+    
+    if st.sidebar.button('Classify'):
+        st.subheader("Support Vector Machine")
+        svc_model = SVC(C = c_value, kernel = kernel_input, gamma = gamma_input)
+        svc_model.fit(X_train,y_train)
+        y_pred = svc_model.predict(X_test)
+        accuracy = svc_model.score(X_test, y_test)
+        glass_type = prediction(svc_model, ri, na, mg, al, si, k, ca, ba, fe)
+        st.write("The Type of glass predicted is:", glass_type)
+        st.write("Accuracy", accuracy.round(2))
+        plot_confusion_matrix(svc_model, X_test, y_test)
+        st.pyplot()
+
+if classifier == 'Random Forest Classifier':
+    st.sidebar.subheader("Model Hyperparameters")
+    n_estimators_input = st.sidebar.number_input("Number of trees in the forest", 100, 5000, step = 10)
+    max_depth_input = st.sidebar.number_input("Maximum depth of the tree", 1, 100, step = 1)
+
+    
+    if st.sidebar.button('Classify'):
+        st.subheader("Random Forest Classifier")
+        rf_clf = RandomForestClassifier(n_estimators = n_estimators_input, max_depth = max_depth_input, n_jobs = -1)
+        rf_clf.fit(X_train,y_train)
+        accuracy = rf_clf.score(X_test, y_test)
+        glass_type = prediction(rf_clf, ri, na, mg, al, si, k, ca, ba, fe)
+        st.write("The Type of glass predicted is:", glass_type)
+        st.write("Accuracy", accuracy.round(2))
+        plot_confusion_matrix(rf_clf, X_test, y_test)
+        st.pyplot()
